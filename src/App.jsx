fbc3bf581903e40dd511e4eba190fc8cafbd3be6
Alon Avisar <1github@proton.me>
2022-12-27 15:12:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <aside className='sidemenu'>
+      <div className='side-menu-btn'>
+        New Chat
+      </div>
+    </aside>
+    <section className='chatbox'>
+
+    </section>
+    </div>
+  );
+}
+
+export default App;
