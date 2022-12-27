23ac43e547866f29386595312099f7f7dff5d3ac
Alon Avisar <1github@proton.me>
2022-12-27 16:30:19 +0200

    adding side bar + chat msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import './App.css';
+import "./App.scss";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
   return (
     <div className="App">
-    <aside className='sidemenu'>
-      <div className='side-menu-btn'>
-        New Chat
-      </div>
-    </aside>
-    <section className='chatbox'>
+      <aside className="sidemenu">
+        <div className="side-menu-btn">
+          <AddIcon />
+          <span> New Chat </span>
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-msg">
+            <div className="chat-msg-center">
 
-    </section>
+            <div className="avatar"></div>
+            <div className="msg">Hw</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-log">
+          <div className="chat-msg chatgpt">
+            <div className="chat-msg-center">
+
+            <div className="avatar"></div>
+            <div className="msg">I am an AI</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="chat-input-holder">
+          <input className="text-input-area"></input>
+        </div>
+      </section>
     </div>
   );
 }
